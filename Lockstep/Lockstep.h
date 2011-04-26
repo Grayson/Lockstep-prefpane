bc0dddd9e7c78db84fecd4b416e10a7d2ddd725b
Grayson Hansard <info@fromconcentratesoftware.com>
2011-04-26 16:04:42 -0400

    Initial commit.

---
+++ Lockstep/Lockstep.h
@@ -0,0 +1,19 @@
+//
+//  Lockstep.h
+//  Lockstep
+//
+//  Created by Grayson Hansard on 4/26/11.
+//  Copyright 2011 From Concentrate Software. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+
+@interface Lockstep : NSPreferencePane {
+@private
+    
+}
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ Lockstep/Lockstep.m
@@ -0,0 +1,18 @@
+//
+//  Lockstep.m
+//  Lockstep
+//
+//  Created by Grayson Hansard on 4/26/11.
+//  Copyright 2011 From Concentrate Software. All rights reserved.
+//
+
+#import "Lockstep.h"
+
+
+@implementation Lockstep
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
